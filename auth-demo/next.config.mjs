35e50ba238c834bf38e5c9ea40cfaa175f3a53cc
Felix <chuanfa.chai@gmail.com>
2024-11-13 14:59:24 +0800

    chrore: update sdk

--- auth-demo/next.config.mjs
+++ auth-demo/next.config.mjs
@@ -1,3 +1,8 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -13,11 +18,17 @@ const nextConfig = {
   //       })
   //     );
   //   } else {
-  //     config.plugins.push(codeInspectorPlugin({ bundler: 'webpack', editor: 'code' }));
+  //     config.plugins.push(codeInspectorPlugyarnin({ bundler: 'webpack', editor: 'code' }));
   //   }
 
   //   return config;
   // },
+
+  webpack: (config) => {
+    config.resolve.alias['react'] = path.resolve(__dirname, '../node_modules/react');
+    config.resolve.alias['react-dom'] = path.resolve(__dirname, '../node_modules/react-dom');
+    return config;
+  },
   transpilePackages: [ "antd", "@ant-design", "rc-util", "rc-pagination", "rc-picker", "rc-notification", "rc-tooltip", "rc-tree", "rc-table" ],
   experimental: {
     esmExternals: false,
